11e5ef20938ec206664d166910ab3b830bc7c771
@nikoli_sytin <kok.74@mail.ru>
2022-05-25 15:47:23 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+// import "fmt"
+var a int
+func changeA(b int) {
+	a = b
+}
+
+// func doPanic(a int) {
+// 	if a > 1 {
+// 		doPanic(a - 1)
+// 	}
+// 	panic(a)
+// }
+func main() {
+	for i := 0; i < 10; i++ {
+		changeA(i)
+	}
+	// fmt.Println("I am debugging now")
+	// doPanic(5)
+}
